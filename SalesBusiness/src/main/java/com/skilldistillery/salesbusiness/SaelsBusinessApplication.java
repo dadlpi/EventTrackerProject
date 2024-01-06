dbd4dd449aafd476e5dc773ad1423841c2afa06c
diego.delapuente35 <diego.a.delapuente@gmail.com>
2024-01-05 16:25:01 -0800

    Configured Spring REST Project, connected JPA project dependency

---
+++ SalesBusiness/src/main/java/com/skilldistillery/salesbusiness/SaelsBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.salesbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaelsBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaelsBusinessApplication.class, args);
+	}
+
+}
---
+++ SalesBusiness/src/test/java/com/skilldistillery/salesbusiness/SaelsBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.salesbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaelsBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
